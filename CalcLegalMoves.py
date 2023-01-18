51e87893eb0eafb1aabfc0b791d14dac4eed5cd7
Quaqiem <jochemvanhouten@gmail.com>
2023-01-18 21:36:49 +0100

    sfdg

---
+++ CalcLegalMoves.py
@@ -0,0 +1,140 @@
+import csv
+
+#120x board importen
+Board120xFile = csv.reader(open('Board120x.csv', 'r'))
+Board120x = [list(map(int, i)) for i in Board120xFile]
+
+#64x board importen
+Board64xFile = csv.reader(open('Board64x.csv', 'r'))
+Board64x = [list(map(int, i)) for i in Board64xFile]
+
+PiecesListFile = list(csv.reader(open('StartPos.csv', 'r')))
+PiecesList = [list(map(int, i)) for i in PiecesListFile]
+
+#Constants
+IsSlidingPiece = [False, False, False, True, True, True, False]
+OffsetAmount = [0, 2, 8, 4, 4, 8, 8]
+OffsetValues = [
+    [0, 0, 0, 0, 0, 0, 0, 0],           #Empty
+    [-11, -9, 9, 11, 0, 0, 0, 0],        #Pawn
+    [-21, -19,-12, -8, 8, 12, 19, 21],  #Pferd
+    [-11, -9, 9, 11, 0, 0, 0, 0],       #Bishop
+    [-10, -1, 1, 10, 0, 0, 0, 0],       #Rook
+    [-11, -10, -9, -1, 1, 9, 10, 11],   #Queen
+    [-11, -10, -9, -1, 1, 9, 10, 11]]   #King
+
+#kijken of pawn zich op eerste rij bevindt, zo ja double push mogelijk
+CheckPawnOnFirstRowW = [81, 82, 83, 84, 85, 86, 87, 88]
+CheckPawnOnFirstRowB = [31, 32, 33, 34, 35, 36, 37, 38]
+
+#Calc pseudo legal moves (alle moves zonder dat er met schaak, en passant of rokeren te maken heeft), moves voor specific pieces komt in andere functie
+def CalcPseudoLegalMoves(BoardConfig, Side, NotSide):
+    LegalMoves = [[],[],[]]
+    for i in range(64):
+        if BoardConfig[1][i] == Side:
+                    Piece = BoardConfig[0][i]
+                    if Piece != 1:
+                        for j in range(OffsetAmount[Piece]):
+                            for m in range(1, 8):
+                                
+                                IndexNumber = Board64x[0][i] + OffsetValues[Piece][j] * m
+
+                                if IndexNumber <= 120:
+                                    n = Board120x[0][IndexNumber]
+                                else:
+                                    n = -1
+
+                                if n == -1: break
+
+                                if BoardConfig[1][n] != 0:
+                                    if BoardConfig[1][n] == NotSide:
+                                        if n not in LegalMoves:
+                                            LegalMoves[0].append(i)
+                                            LegalMoves[1].append(n)
+                                            LegalMoves[2].append(True)
+                                        break
+                                    else:
+                                        break
+                                        
+                                elif BoardConfig[1][n] == 0:
+                                    if n not in LegalMoves:
+                                        LegalMoves[0].append(i)
+                                        LegalMoves[1].append(n)
+                                        LegalMoves[2].append(False)
+                                    breakpoint
+                                    
+                                if IsSlidingPiece[Piece] == False: break                  
+                    else:
+                        #Double pawn moves
+                        if Board64x[0][i] in CheckPawnOnFirstRowW or Board64x[0][i] in CheckPawnOnFirstRowB:
+                            if BoardConfig[1][i] == 1:
+                                IndexNumber = Board64x[0][i] - 20
+                                n = Board120x[0][IndexNumber]
+                            elif BoardConfig[1][i] == 2:
+                                IndexNumber = Board64x[0][i] + 20
+                                n = Board120x[0][IndexNumber]
+
+                            if IndexNumber == -1: break
+
+                            if BoardConfig[1][n] == 0:
+                                LegalMoves[0].append(i)
+                                LegalMoves[1].append(n)
+                                LegalMoves[2].append(False)
+                            else: breakpoint
+                        
+                        #Normale pawn moves
+                        if BoardConfig[1][i] == 1:
+                            IndexNumber = Board64x[0][i] - 10
+                            n = Board120x[0][IndexNumber]   
+                        else:
+                            IndexNumber = Board64x[0][i] + 10
+                            n = Board120x[0][IndexNumber]
+                        
+                        if BoardConfig[1][n] == 0:
+                            LegalMoves[0].append(i)
+                            LegalMoves[1].append(n)
+                            LegalMoves[2].append(False)
+                        
+                        else: breakpoint
+
+                        #Pawn captures
+                        for j in range(OffsetAmount[Piece]):
+                            if BoardConfig[1][i] == 1:
+                                IndexNumber = Board64x[0][i] + OffsetValues[Piece][j]
+                            else:
+                                IndexNumber = Board64x[0][i] + OffsetValues[Piece][j + 2]
+                            if IndexNumber <= 120:
+                                n = Board120x[0][IndexNumber]
+                            else:
+                                n = -1
+                            if n == -1: break
+                            
+                            if BoardConfig[1][n] != 0:
+                                if BoardConfig[1][n] == NotSide:
+                                    if n not in LegalMoves:
+                                        LegalMoves[0].append(i)
+                                        LegalMoves[1].append(n)
+                                        LegalMoves[2].append(True)            
+    return LegalMoves
+
+
+
+def PieceSpecificMoves(SelectedPiece, LegalMoves):
+    PieceSpecificMove = [[],[],[]]
+    LegalMoves = list(LegalMoves)
+    i = SelectedPiece
+    print(i)
+    print(len(LegalMoves[0]))
+
+    while i in LegalMoves[0]: 
+        for j in range(3):
+            LegalMoves[j].append(LegalMoves[j])
+            break
+    
+    
+    return PieceSpecificMove
+
+
+
+
+
--- jogmklote.py
+++ jogmklote.py
@@ -3,8 +3,10 @@ import os
 import csv
 import math
 import time
+import CalcLegalMoves
 
 python = sys.executable
+clear = lambda: os.system('cls')
 
 #Bord om te kijken of het stuk op het speelbord blijft, was sneller volgens gekke website
 Board120xFile = csv.reader(open('Board120x.csv', 'r'))
@@ -18,7 +20,7 @@ Board64x = [list(map(int, i)) for i in Board64xFile]
 #Eerste is Pieces, 2de is kleuren
 # 0 = Empty, 1 = Pawn, 2 = Knight, 3 = Bishop, 4 = Rook, 5 = Queen, 6 = King
 # 0 = Empty, 1 = White, 2 = Black
-PiecesListFile = list(csv.reader(open('Test Bord.csv', 'r')))
+PiecesListFile = list(csv.reader(open('StartPos.csv', 'r')))
 PiecesList = [list(map(int, i)) for i in PiecesListFile]
 
 #Van bordcoordinaat naar x64index int
@@ -46,16 +48,6 @@ def MakeMove(InputFromSquare, InputToSquare):
     PiecesList[0][From] = 0
     PiecesList[1][From] = 0
 
-IsSlidingPiece = [False, False, False, True, True, True, False]
-OffsetAmount = [0, 0, 8, 4, 4, 8, 8]
-OffsetValues = [
-    [0, 0, 0, 0, 0, 0, 0, 0],           #Empty
-    [0, 0, 0, 0, 0, 0, 0, 0],           #Pawwn
-    [-21, -19,-12, -8, 8, 12, 19, 21],  #Pferd
-    [-11, -9, 9, 11, 0, 0, 0, 0],       #Bishop
-    [-10, -1, 1, 10, 0, 0, 0, 0],       #Rook
-    [-11, -10, -9, -1, 1, 9, 10, 11],   #Queen
-    [-11, -10, -9, -1, 1, 9, 10, 11]]   #King
 
 PiecesDict = {
         #stukken
@@ -80,7 +72,6 @@ NotSide = 2
 while True:
     StartTime = time.process_time()
 
-
     PrintGrid = ["."] * 64
 
     SelectPiece = False
@@ -88,7 +79,12 @@ while True:
 
         LegalMoves = [[],[],[]]
 
-        print(Side)
+        if Side == 1:
+            print("White")
+        else:
+            print("Black")
+
+        print()
 
         #printen van bord
         for i in range(8):
@@ -100,51 +96,24 @@ while True:
                 
             print()
 
-        print('\n')
+        print()
     
         InputFrom = SquareNumb(input("From: "))
 
+        print()
+
+
         #Pseudo legal move generenen
-        if PiecesList[1][InputFrom] == Side:
-            Piece = PiecesList[0][InputFrom]
-            if Piece != 1:
-                for j in range(OffsetAmount[Piece]):
-                    for m in range(1, 8):
-                        
-                        IndexNumber = Board64x[0][InputFrom] + OffsetValues[Piece][j] * m
+        LegalMoves = CalcLegalMoves.PieceSpecificMoves(InputFrom, CalcLegalMoves.CalcPseudoLegalMoves(PiecesList, Side, NotSide))
+        
+        print(LegalMoves)
 
-                        if IndexNumber <= 120:
-                            n = Board120x[0][IndexNumber]
-                        else:
-                            n = -1
-
-                        if n == -1: break
-
-                        if PiecesList[1][n] != 0:
-                            if PiecesList[1][n] == NotSide:
-                                if n not in LegalMoves:
-                                    LegalMoves[0].append(i)
-                                    LegalMoves[1].append(n)
-                                    LegalMoves[2].append(True)
-                                break
-                                
-                        elif PiecesList[1][n] == 0:
-                            if n not in LegalMoves:
-                                LegalMoves[0].append(i)
-                                LegalMoves[1].append(n)
-                                LegalMoves[2].append(False)
-                            breakpoint
-                            
-                        if IsSlidingPiece[Piece] == False: break                  
-            else:
-                None #pawn moves
-
-        for j in range(len(LegalMoves[0])):
-            ConvertLegalMoves = [[],[],[]]
-            ConvertLegalMoves[0] = InvSquareNumb(int(LegalMoves[0][j]))
-            ConvertLegalMoves[1] = InvSquareNumb(int(LegalMoves[1][j]))
-            ConvertLegalMoves[2] = LegalMoves[2][j]
-            print(ConvertLegalMoves)
+        #for j in range(len(LegalMoves[0])):
+         #   ConvertLegalMoves = [[],[],[]]
+         #   ConvertLegalMoves[0] = InvSquareNumb(int(LegalMoves[0][j]))
+          #  ConvertLegalMoves[1] = InvSquareNumb(int(LegalMoves[1][j]))
+          #  ConvertLegalMoves[2] = LegalMoves[2][j]
+           # print(ConvertLegalMoves)
 
 
         #printen legalmoves & capture
@@ -154,26 +123,25 @@ while True:
                 PrintGrid[LegalMovesPrint[i]] = 'x'
             else:
                 PrintGrid[LegalMovesPrint[i]] = 'o'
-            
+        
+        print()
             
         for i in range(0, 64, 8):
             print(' '.join(PrintGrid[i:i+8]))
                 
         PrintGrid = ["."] * 64
         
-        InputContinue = input("Move this piece? Y/N ")
-        if InputContinue.lower() == 'y':
-            SelectPiece = True
-        else:
-            SelectPiece = False
-            LegalMoves[0].clear()
-            LegalMoves[1].clear()
-            LegalMoves[2].clear()
-            
+        print()
+
+        SelectPiece = True
+        
     ChooseMove = False
     while ChooseMove == False:
+        print()
 
         InputTo = SquareNumb(input("to: "))
+
+        print()
 
         if InputTo in LegalMoves[1]:
             MakeMove(InputFrom, InputTo)
@@ -188,5 +156,9 @@ while True:
 
     EndTime = time.process_time()
     print("Process time: ", EndTime - StartTime)
+    print()
+    clear()
+    
 
     
+ 
