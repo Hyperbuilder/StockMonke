eaf44f0ca8eacc4ec2e73e0c64373ddc59977e1a
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2023-01-18 17:53:01 +0100

    u Stupid thats big nono

--- jogmklote.py
+++ jogmklote.py
@@ -21,70 +21,102 @@ Board64x = [list(map(int, i)) for i in Board64xFile]
 PiecesListFile = list(csv.reader(open('Test Bord.csv', 'r')))
 PiecesList = [list(map(int, i)) for i in PiecesListFile]
 
+#Van bordcoordinaat naar x64index int
+def SquareNumb(Square):
+    File = ord(Square[0]) - ord('a')
+    Rank = 8 - int(Square[1])
+    return Rank * 8 + File
 
+#Van x64index int naar bordcoordinaat
+def InvSquareNumb(Square):
+    FileNum= Square % 8
+    RankNum = 8 - math.floor(Square / 8)
+    File = chr((ord('a') + FileNum))
+    Rank = str(RankNum)
+    return File + Rank
+
+
+def MakeMove(InputFromSquare, InputToSquare):
+    From = InputFromSquare
+    To = InputToSquare
+
+    PiecesList[0][To] = PiecesList[0][From]
+    PiecesList[1][To] = PiecesList[1][From]
+
+    PiecesList[0][From] = 0
+    PiecesList[1][From] = 0
+
+IsSlidingPiece = [False, False, False, True, True, True, False]
+OffsetAmount = [0, 0, 8, 4, 4, 8, 8]
+OffsetValues = [
+    [0, 0, 0, 0, 0, 0, 0, 0],           #Empty
+    [0, 0, 0, 0, 0, 0, 0, 0],           #Pawwn
+    [-21, -19,-12, -8, 8, 12, 19, 21],  #Pferd
+    [-11, -9, 9, 11, 0, 0, 0, 0],       #Bishop
+    [-10, -1, 1, 10, 0, 0, 0, 0],       #Rook
+    [-11, -10, -9, -1, 1, 9, 10, 11],   #Queen
+    [-11, -10, -9, -1, 1, 9, 10, 11]]   #King
+
+PiecesDict = {
+        #stukken
+        0 : '.',
+        1 : 'P',
+        2 : 'N',
+        3 : 'B',
+        4 : 'R', 
+        5 : 'Q',
+        6 : 'K',
+
+        #kleuren
+        7 : '.',
+        8 : 'W',
+        9 : 'B'
+    }
+
+#Wiens beurt
+Side = 1
+NotSide = 2
 
 while True:
-
     StartTime = time.process_time()
 
-    #eerste is vanaf 2de is naartoe, 3de is capture of niet
-    LegalMoves = [[],[],[]]
 
-    #input player
-    InputFrom = input("from: ")
-    InputTo = input("to: ")
+    PrintGrid = ["."] * 64
 
-    #Van vakje naar de array zodat je f1 in kan voeren enzo
-    def SquareNumb(Square):
-        File = ord(Square[0]) - ord('a')
-        Rank = 8 - int(Square[1])
-        return Rank * 8 + File
+    SelectPiece = False
+    while SelectPiece == False:     
 
-    def InvSquareNumb(Square):
-        FileNum= Square % 8
-        RankNum = 8 - math.floor(Square / 8)
-        
-        File = chr((ord('a') + FileNum))
-        Rank = str(RankNum)
+        LegalMoves = [[],[],[]]
 
-        return File + Rank
+        print(Side)
 
-    print(SquareNumb(InputFrom))
+        #printen van bord
+        for i in range(8):
+            for j in range(8):
+                Piece = PiecesList[0][i * 8 + j]
+                Color = PiecesList[1][i * 8 + j] + 7
+                
+                print(PiecesDict[Piece], PiecesDict[Color], sep='', end= ' ')
+                
+            print()
 
-    #Legal move berekenen
+        print('\n')
+    
+        InputFrom = SquareNumb(input("From: "))
 
-    #kijken of move op bord is
-    #wiens beurt het is, begint natuurlijk bij wit
-    Side = 1
-    NotSide = 2
-
-    IsSlidingPiece = [False, False, False, True, True, True, False]
-    OffsetAmount = [0, 0, 8, 4, 4, 8, 8]
-    OffsetValues = [
-        [0, 0, 0, 0, 0, 0, 0, 0],           #Empty
-        [0, 0, 0, 0, 0, 0, 0, 0],           #Pawwn
-        [-21, -19,-12, -8, 8, 12, 19, 21],  #Pferd
-        [-11, -9, 9, 11, 0, 0, 0, 0],       #Bishop
-        [-10, -1, 1, 10, 0, 0, 0, 0],       #Rook
-        [-11, -10, -9, -1, 1, 9, 10, 11],   #Queen
-        [-11, -10, -9, -1, 1, 9, 10, 11]]   #King
-
-    for i in range(64):
-        if PiecesList[1][i] == Side:
-            Piece = PiecesList[0][i]
+        #Pseudo legal move generenen
+        if PiecesList[1][InputFrom] == Side:
+            Piece = PiecesList[0][InputFrom]
             if Piece != 1:
                 for j in range(OffsetAmount[Piece]):
-                    n = i
-                    for m in range(1, 10):
+                    for m in range(1, 8):
                         
-                        IndexNumber = Board64x[0][n] + OffsetValues[Piece][j] * m
+                        IndexNumber = Board64x[0][InputFrom] + OffsetValues[Piece][j] * m
 
                         if IndexNumber <= 120:
                             n = Board120x[0][IndexNumber]
                         else:
                             n = -1
-
-                        print(IndexNumber, "m:",m, "n:",n)
 
                         if n == -1: break
 
@@ -94,7 +126,7 @@ while True:
                                     LegalMoves[0].append(i)
                                     LegalMoves[1].append(n)
                                     LegalMoves[2].append(True)
-                                breakpoint
+                                break
                                 
                         elif PiecesList[1][n] == 0:
                             if n not in LegalMoves:
@@ -103,59 +135,56 @@ while True:
                                 LegalMoves[2].append(False)
                             breakpoint
                             
-                        
                         if IsSlidingPiece[Piece] == False: break                  
             else:
                 None #pawn moves
-        print(len(LegalMoves[0]))
+
+        for j in range(len(LegalMoves[0])):
+            ConvertLegalMoves = [[],[],[]]
+            ConvertLegalMoves[0] = InvSquareNumb(int(LegalMoves[0][j]))
+            ConvertLegalMoves[1] = InvSquareNumb(int(LegalMoves[1][j]))
+            ConvertLegalMoves[2] = LegalMoves[2][j]
+            print(ConvertLegalMoves)
+
+
+        #printen legalmoves & capture
+        for i in range(len(LegalMoves[1])):
+            LegalMovesPrint = LegalMoves[1]
+            if LegalMoves[2][i] == False:
+                PrintGrid[LegalMovesPrint[i]] = 'x'
+            else:
+                PrintGrid[LegalMovesPrint[i]] = 'o'
             
-    
-    for j in range(len(LegalMoves[0])):
-        ConvertLegalMoves = [[], [], []]
-        ConvertLegalMoves[0] = InvSquareNumb(int(LegalMoves[0][j]))
-        ConvertLegalMoves[1] = InvSquareNumb(int(LegalMoves[1][j]))
-        ConvertLegalMoves[2] = LegalMoves[2][j]
-        print(ConvertLegalMoves)
-    
-
-    #move maak klote
-    def MakeMove(InputFromSquare, InputToSquare):
-        From = SquareNumb(InputFromSquare)
-        To = SquareNumb(InputToSquare)
-
-        PiecesList[0][To] = PiecesList[0][From]
-        PiecesList[1][To] = PiecesList[1][From]
-
-        PiecesList[0][From] = 0
-        PiecesList[1][From] = 0
-
-    MakeMove(InputFrom, InputTo)
-
-    #printen in consoleS
-    PiecesDict = {
-        #stukken
-        0 : ' ',
-        1 : 'P',
-        2 : 'N',
-        3 : 'B',
-        4 : 'R', 
-        5 : 'Q',
-        6 : 'K',
-
-        #kleuren
-        7 : ' ',
-        8 : 'W',
-        9 : 'B'
-    }
-
-    for i in range(8):
-        for j in range(8):
-            Piece = PiecesList[0][i * 8 + j]
-            Color = PiecesList[1][i * 8 + j] + 7
             
-            print(PiecesDict[Piece], PiecesDict[Color], sep='', end= ' ')
+        for i in range(0, 64, 8):
+            print(' '.join(PrintGrid[i:i+8]))
+                
+        PrintGrid = ["."] * 64
+        
+        InputContinue = input("Move this piece? Y/N ")
+        if InputContinue.lower() == 'y':
+            SelectPiece = True
+        else:
+            SelectPiece = False
+            LegalMoves[0].clear()
+            LegalMoves[1].clear()
+            LegalMoves[2].clear()
             
-        print()
+    ChooseMove = False
+    while ChooseMove == False:
+
+        InputTo = SquareNumb(input("to: "))
+
+        if InputTo in LegalMoves[1]:
+            MakeMove(InputFrom, InputTo)
+            ChooseMove = True
+        else:
+            ChooseMove = False 
+            print('\n', "u stupid thats big nono", '\n')
+
+    StoreSide = Side
+    Side = NotSide 
+    NotSide = StoreSide    
 
     EndTime = time.process_time()
     print("Process time: ", EndTime - StartTime)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame as pygame
-
+import sys
+import os
+import csv
+import math
+#import jogmklote as engine
 
 #Standaard waarden voor venster en schaakbord
 width = 1024
@@ -10,13 +14,33 @@ squareWidth = width / xDimension
 squareHeight = height / yDimension
 maxFramesPerSecond = 30
 
+PiecesDict = {
+    #stukken
+    0 : ' ',
+    1 : 'P',
+    2 : 'N',
+    3 : 'B',
+    4 : 'R', 
+    5 : 'Q',
+    6 : 'K',
+
+    #kleuren
+    7 : ' ',
+    8 : 'W',
+    9 : 'B'
+}
+
+colorThemes = [
+    [pygame.Color("white"), pygame.Color("darkgray")],
+    [pygame.Color("white"), pygame.Color("purple")],
+]
+selectedBoardTheme = colorThemes[0]
 
 xBoardCoordinates = ["a", "b", "c", "d", "e", "f", "g", "h"]
 yBoardCoordinates = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 #Load image with "images['piece']"
 images = {}
-
 #Functie laad images 1x in.
 def imageLoader():
     pieces = ['PW', 'RW', 'NW', 'BW', 'QW', 'KW', 'PB', 'RB', 'NB', 'BB', 'QB', 'KB']
@@ -25,56 +49,102 @@ def imageLoader():
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((width,height), #pygame.NOFRAME
-    );
+    screen = pygame.display.set_mode((width,height));
     clock = pygame.time.Clock()
     gameRunning = True
-    
     sysfont = pygame.font.get_default_font()
     fontSize = int(squareHeight//3)
     font = pygame.font.SysFont(sysfont, fontSize)
 
     imageLoader()
 
+    SelectedSquareLocation = ''
+    playerSelectedSquares = []
+    pieceOnSelectedSquare = ''
+
+
     while gameRunning:
+
+        PiecesListFile = list(csv.reader(open('Test bord.csv', 'r')))
+        PiecesList = [list(map(int, i)) for i in PiecesListFile]
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameRunning = False
                 print("Window closed")
-                    
+            elif event.type == pygame.MOUSEBUTTONDOWN:
 
-        drawScreen(screen, font)
+                mouseBoardLocation = getBoardLocationCoords()
+                if mouseBoardLocation == SelectedSquareLocation:
+                    SelectedSquareLocation = ''
+                    playerSelectedSquares = []
+                    pieceOnSelectedSquare = ''
+                elif len(playerSelectedSquares) == 0:
+                    SelectedSquareLocation = mouseBoardLocation
+                    playerSelectedSquares.append(mouseBoardLocation)
+                    pieceOnSelectedSquare = getChessPieceOnLocation(PiecesList)
+                elif len(playerSelectedSquares) == 1:
+                    SelectedSquareLocation = ''
+                    playerSelectedSquares.append(mouseBoardLocation)
+                    #engine.MakeMove(playerSelectedSquares[0], playerSelectedSquares[1])
+                    print("moved " + pieceOnSelectedSquare + playerSelectedSquares[0] + " to " + playerSelectedSquares[1])
+                    playerSelectedSquares = []
+                    pieceOnSelectedSquare = ''
+
+
+        drawScreen(screen, font, selectedBoardTheme, PiecesList)
         clock.tick(maxFramesPerSecond)
         pygame.display.flip()
 
-def drawScreen(screen, font):
-    drawChessBoard(screen, font)
+def drawScreen(screen, font, selectedBoardTheme, PiecesList):
+    drawChessBoard(screen, font, selectedBoardTheme)
+    drawChessPieces(screen, PiecesList)
 
-def drawChessBoard(screen, font):
-    colorThemes = [
-        [pygame.Color("white"), pygame.Color("darkgray")],
-        [pygame.Color("white"), pygame.Color("purple")],
-    ]
-
+def drawChessBoard(screen, font, BoardTheme):
     for row in range(8):
         for column in range(8):
-            color = colorThemes[0][((row+column) % 2)]
+            color = BoardTheme[((row+column) % 2)]
             pygame.draw.rect(screen, color, pygame.Rect(column*squareWidth, row*squareHeight, squareWidth, squareHeight))
             if row == 7:
-                coordinateText = font.render(xBoardCoordinates[column], False, (200, 200, 200))
+                coordinateText = font.render(xBoardCoordinates[column], False, (200, 200, 200)).convert()
+                #change -21 and -30 to relative to screensize values
                 screen.blit(coordinateText, (column*squareWidth + squareWidth - 21, row*squareHeight + squareHeight - 30))
             elif column == 0:
-                coordinateText = font.render(yBoardCoordinates[row], False, (200, 200, 200))
+                coordinateText = font.render(yBoardCoordinates[7 - row], False, (200, 200, 200))
                 screen.blit(coordinateText, (column*squareWidth + 3, row*squareHeight + 3))
 
-
-
-def drawChessPieces(screen, board):
+def drawChessPieces(screen, PiecesList):
     for row in range(yDimension):
         for column in range(xDimension):
-            piece = board[row][column]
-            if piece != "--":
+            #CSV data omzetten naar letters
+            Piece = PiecesList[0][row * 8 + column]
+            Color = PiecesList[1][row * 8 + column] + 7
+            #Letters mergen waardoor images[] het kan gebruiken
+            piece = str(PiecesDict[Piece]) + str(PiecesDict[Color])
+            if piece != '  ':
                 screen.blit(images[piece], pygame.Rect(column*squareWidth, row*squareHeight, squareWidth, squareHeight))
+
+def getBoardLocationCoords():
+    mouseLocationCoords = pygame.mouse.get_pos()
+    mouseXAxisLocation = mouseLocationCoords[0]
+    mouseYAxisLocation = mouseLocationCoords[1]
+    mouseBoardLocation = str(xBoardCoordinates[mouseXAxisLocation // (width//8)]) + str(yBoardCoordinates[7 - mouseYAxisLocation // (height//8)])
+    return mouseBoardLocation
+
+def SquareIndexNumber(Square):
+    File = ord(Square[0]) - ord('a')
+    Rank = 8 - int(Square[1])
+    return Rank * 8 + File
+
+def getChessPieceOnLocation(PiecesList):
+    mouseBoardCoordinate = getBoardLocationCoords()
+    x64BoardindexNumber = SquareIndexNumber(mouseBoardCoordinate)
+    PieceIndexNumber = PiecesList[0][x64BoardindexNumber]
+    return PiecesDict[PieceIndexNumber]
+
+
+
+
 
 
 if __name__ == "__main__":
