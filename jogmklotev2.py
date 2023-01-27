9be59cdee17e482679a4adaa5e44a1093fa4e901
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2023-01-27 17:49:33 +0100

    Perft debug + 1st test

--- jogmklotev2.py
+++ jogmklotev2.py
@@ -11,6 +11,8 @@ BoardConfig = [list(map(int, i)) for i in Functions.ConvertFENString(FEN)[0]]
 
 WhiteToMove = Functions.ConvertFENString(FEN)[1]
 
+depth = int(input("Depth: "))
+
 
 #CalcLegalMoves.FinalLegalMoves(BoardConfig, WhiteToMove)
 
@@ -27,7 +29,7 @@ def perft(depth, BoardConfig, WhiteToMove):
 
 	if depth % 2 != 0 and depth > 0:
 		Side = WhiteToMove
-	elif depth > 0:
+	elif depth % 2 == 0 and depth > 0:
 		Side = not WhiteToMove
 	
 
@@ -41,19 +43,31 @@ def perft(depth, BoardConfig, WhiteToMove):
 			
 			BoardConfigReset = [x[:] for x in BoardConfig]
 
-
 			BoardConfigReset = Functions.MakeMove(legalmoves[0][move], legalmoves[1][move], BoardConfigReset, legalmoves)
-			count += perft(depth - 1, BoardConfigReset, not Side)
-
-			
-			
-			
-
+			result = perft(depth - 1, BoardConfigReset, Side)
+			count += result
+			print(legalmoves[0][move], count, result)
 		return count
 
 
 start = time.perf_counter()
-print(perft(5, BoardConfig, WhiteToMove))
+perftresult = perft(depth, BoardConfig, WhiteToMove)
 end = time.perf_counter()
-print(str((end - start) * 1000) + " ms" )
+print(perftresult, str((end - start) * 1000) + " ms")
+if depth == 1: 
+	print(str(20-perftresult) + " missing, expected 20 recieved: " + str(perftresult))
+elif depth == 2:
+	print(str(400-perftresult) + " missing, expected 400 recieved: " + str(perftresult))
+elif depth == 3:
+	print(str(8902-perftresult) + " missing, expected 8902 recieved: " + str(perftresult))
+elif depth == 4:
+	print(str(197281-perftresult) + " missing, expected 197281 recieved: " + str(perftresult))
+elif depth == 5:
+	print(str(4865609-perftresult) + " missing, expected 4865609 recieved: " + str(perftresult))
 
+#27-1-2023 17:43 Intel Core i7-8565U: 
+# Depth 1: 20			te verwaarlozen				Correct
+# Depth 2: 400			te verwaarlozen				Correct
+# Depth 3: 8864  		691.5813000014168 ms		38 missing, expected 8902 recieved: 8864
+# Depth 4: 197217		16530.18570000131 ms		64 missing, expected 197281 recieved: 197217
+# Depth 5: 4844186		371445.6950999993 ms 		21423 missing, expected 4865609 recieved: 4844186
